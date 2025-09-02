f9cb70f865a2a166543242e73d00850336ec8ff4
Elias Kleppinger <e.kleppinger@proton.me>
2025-09-02 21:46:26 +0200

    feat: add database enumerations and constants

---
+++ src/database/enums.ts
@@ -0,0 +1,16 @@
+export enum MarketFranchise {
+    REWE = "REWE",
+    PENNY = "Penny",
+    NETTO = "Netto",
+    LIDL = "Lidl",
+    KAUFLAND = "Kaufland",
+    ALDI_NORD = "Aldi Nord",
+    ALDI_SUED = "Aldi Süd",
+    NORMA = "Norma"
+}
+
+export enum UserGender {
+    MALE = "M",
+    FEMALE = "F",
+    OTHER = "O"
+}
